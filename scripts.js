d843d7f1c8cb7b88fc92a8f492cff695920ff8ef
Caleb King <kingcaleb216@gmail.com>
2024-05-25 19:26:49 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var collapsibles = document.querySelectorAll(".collapsible");
+
+    collapsibles.forEach(function(collapsible) {
+        collapsible.addEventListener("click", function() {
+            console.log("Collapsible clicked:", this);
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            var icon = this.querySelector(".icon");
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                icon.textContent = "+"
+            } else {
+                content.style.display = "block";
+                icon.textContent = "-"
+            }
+        });
+    });
+});
+
