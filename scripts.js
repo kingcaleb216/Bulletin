243916fe46a8d5e936c9ac9236c25039fc5b9bbc
Caleb King <kingcaleb216@gmail.com>
2024-05-27 08:11:24 -0500

    Fixing hover issue

--- scripts.js
+++ scripts.js
@@ -3,16 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     collapsibles.forEach(function(collapsible) {
         collapsible.addEventListener("click", function() {
-            console.log("Collapsible clicked:", this);
             this.classList.toggle("active");
             var content = this.nextElementSibling;
             var icon = this.querySelector(".icon");
             if (content.style.display === "block") {
                 content.style.display = "none";
-                icon.textContent = "+"
+                icon.textContent = "+";
             } else {
                 content.style.display = "block";
-                icon.textContent = "-"
+                icon.textContent = "-";
             }
         });
     });
